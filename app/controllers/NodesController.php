94f26f26ca73a3716c5a38b746d73efc70750c77
Gastón Heim <heim.ga@gmail.com>
2014-03-15 20:46:23 -0500

    Controlador para los Nodos.

---
+++ app/controllers/NodesController.php
@@ -0,0 +1,75 @@
+<?php
+
+class NodesController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+		return Nodes::all()->toJson();
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+		$input = Input::all();
+
+		$node = Nodes::create($input);
+
+		return Response::json($node);
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+		$node = Nodes::find($id);
+
+		return Response::json($node);
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+		$input = Input::all();
+
+		$node = Nodes::find($id)->update($input);
+
+		return Response::json($node);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+		$node = Nodes::destroy($id);
+
+		return Response::json($node);
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,11 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+Route::group(array('prefix' => 'api'), function()
+{
+    //
+    Route::resource('nodes', 'NodesController');
+});
+
